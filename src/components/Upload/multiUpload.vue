251ebf767f0a307af7a3b9e790bf8f8ffb4032c1
dongminghe <dongming1.he@gmail.com>
2022-10-28 18:43:01 +0800

    oss 更改

--- src/components/Upload/multiUpload.vue
+++ src/components/Upload/multiUpload.vue
@@ -46,7 +46,7 @@
         dialogVisible: false,
         dialogImageUrl:null,
         useOss:true, //使用oss->true;使用MinIO->false
-        ossUploadUrl:'http://hfc-oss.oss-cn-shanghai.aliyuncs.com',
+        ossUploadUrl:'http://cifs.oss-cn-hangzhou.aliyuncs.com',
         minioUploadUrl:'http://localhost:8080/minio/upload',
       };
     },
--- src/components/Upload/singleUpload.vue
+++ src/components/Upload/singleUpload.vue
@@ -64,7 +64,7 @@
         },
         dialogVisible: false,
         useOss:true, //使用oss->true;使用MinIO->false
-        ossUploadUrl:'http://hfc-oss.oss-cn-shanghai.aliyuncs.com',
+        ossUploadUrl:'http://cifs.oss-cn-hangzhou.aliyuncs.com',
         minioUploadUrl:'http://localhost:8080/minio/upload',
       };
     },
